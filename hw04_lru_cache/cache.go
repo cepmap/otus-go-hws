0c395b73c6efd8070f780f42038aa2e1c46eca4d
cepmap <cepmapp@gmail.com>
2024-10-26 16:16:44 +0300

    made hw04

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -8,9 +10,13 @@ type Cache interface {
 	Clear()
 }
 
-type lruCache struct {
-	Cache // Remove me after realization.
+type Item struct {
+	Key   Key
+	Value interface{}
+}
 
+type lruCache struct {
+	sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -22,4 +28,41 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.Lock()
+	defer c.Unlock()
+
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+		item.Value.(*Item).Value = value
+		return true
+	}
+	if c.queue.Len() >= c.capacity {
+		removeItem := c.queue.Back()
+		c.queue.Remove(removeItem)
+		delete(c.items, removeItem.Value.(*Item).Key)
+	}
+	c.items[key] = c.queue.PushFront(&Item{key, value})
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.Lock()
+	defer c.Unlock()
+
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+		return item.Value.(*Item).Value, true
+	}
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.Lock()
+	defer c.Unlock()
+
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
