ca2eee36e94dff2993a334e8978f7187020304a0
cepmap <cepmapp@gmail.com>
2024-11-04 00:57:57 +0300

    done hw05

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,63 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type errCounter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func (e *errCounter) Add() {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	e.value++
+}
+
+func (e *errCounter) Get() int {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	return e.value
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	// n - max workers
+	// m - max errors
+	if m < 0 {
+		m = 0
+	}
+	errCounter := errCounter{}
+	tasksChannel := make(chan Task, len(tasks))
+	for i := 0; i < len(tasks); i++ {
+		tasksChannel <- tasks[i]
+	}
+	close(tasksChannel)
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for task := range tasksChannel {
+				if errCounter.Get() > n {
+					return
+				} else if task() != nil {
+					errCounter.Add()
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+	if errCounter.Get() < n+m {
+		ErrErrorsLimitExceeded = nil
+	}
+	return ErrErrorsLimitExceeded
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -37,6 +37,31 @@ func TestRun(t *testing.T) {
 		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
+	t.Run("same as a previous, but with large amount of tasks and workers and randomizing errors", func(t *testing.T) {
+		tasksCount := 1000
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				if rand.Intn(100) > 30 {
+					return err
+				}
+				return nil
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 30
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
+	})
 
 	t.Run("tasks without errors", func(t *testing.T) {
 		tasksCount := 50
@@ -66,5 +91,57 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+	t.Run("max errors less than zero", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := -2
+
+		start := time.Now()
+		err := Run(tasks, workersCount, maxErrorsCount)
+		elapsedTime := time.Since(start)
+		require.NoError(t, err)
+
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+	t.Run("tasks without errors without sleep", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 2
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+
+		require.Eventually(t, func() bool {
+			return atomic.LoadInt32(&runTasksCount) == int32(tasksCount)
+		}, 500*time.Millisecond, 10*time.Millisecond, "not all tasks were completed")
 	})
 }
