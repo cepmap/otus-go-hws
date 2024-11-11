488f505f263730135cc04393cb3045ce710a6014
cepmap <cepmapp@gmail.com>
2024-11-11 21:49:59 +0300

    tbd

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"errors"
+	"io"
+	"log"
+	"os"
 )
+
+//go run main.go copy.go -from testdata/input.txt -limit 0 -offset 0 -to testdata/output.txt
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
@@ -10,6 +15,40 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	infile, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer func(ifile *os.File) {
+		err := ifile.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(infile)
+
+	nfile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer nfile.Close()
+
+	infileStat, err := infile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if offset > infileStat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if infileStat.Size() < limit {
+		limit = infileStat.Size()
+	}
+
+	_, err = infile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
