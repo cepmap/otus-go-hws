e8364c4d09c422b8815c96a3a2271f25bc64cbe4
cepmap <cepmapp@gmail.com>
2024-12-22 15:21:47 +0300

    made hw08

---
+++ go.mod
@@ -0,0 +1,3 @@
+module otus-go-hws
+
+go 1.22
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"errors"
+	"log"
+	"os"
+)
+
+var ErrNoArgsProvided = errors.New("no args provided")
+
 func main() {
-	// Place your code here.
+	// В условии явно не указано, ну или я не вижу. Считаем что аргумент с путем до папки и командой обязательны.
+	if len(os.Args) <= 3 {
+		log.Fatal(ErrNoArgsProvided)
+	}
+	parsedEnvs, err := ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	returnCode := RunCmd(os.Args[2:], parsedEnvs)
+	os.Exit(returnCode)
 }
