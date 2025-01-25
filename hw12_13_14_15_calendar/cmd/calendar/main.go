8c11fdbfba0f0ee1f76d4705e9506b8b84823608
cepmap <cepmapp@gmail.com>
2025-01-25 14:51:48 +0300

    push calendar b4 lints

--- hw12_13_14_15_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_calendar/cmd/calendar/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
-	flag "github.com/spf13/pflag"
+	"fmt"
+	"github.com/cepmap/otus-go-hws/hw12_13_14_15_calendar/internal/config"
+	sqlstorage "github.com/cepmap/otus-go-hws/hw12_13_14_15_calendar/internal/storage/sql"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,33 +13,40 @@ import (
 	"github.com/cepmap/otus-go-hws/hw12_13_14_15_calendar/internal/app"
 	"github.com/cepmap/otus-go-hws/hw12_13_14_15_calendar/internal/logger"
 	internalhttp "github.com/cepmap/otus-go-hws/hw12_13_14_15_calendar/internal/server/http"
-	memorystorage "github.com/cepmap/otus-go-hws/hw12_13_14_15_calendar/internal/storage/memory"
-	//sqlstorage "github.com/cepmap/otus-go-hws/hw12_13_14_15_calendar/internal/storage/sql"
 )
 
 var configFile string
+var AppConfig config.Config
 
-func init() {
-	flag.StringVarP(&configFile, "config", "c", "config.yaml", "config file")
-}
+//func init() {
+//	flag.StringVarP(&configFile, "config", "c", "config.yaml", "config file")
+//}
 
 func main() {
-	flag.Parse()
-
-	if flag.Arg(0) == "version" {
+	if config.Settings.PrintVersion {
 		printVersion()
 		return
 	}
 
-	config := NewConfig(configFile)
+	logger.SetLogLevel(config.Settings.Log.Level)
+	logger.Debug(config.Settings.DebugMessage)
 
-	logg := logger.New(config.Logger.Level)
+	AppConfig.Storage.Type = "memory"
 
-	storage := memorystorage.New()
+	var storage app.Storage
+	switch config.Settings.Storage.Type {
+	case "memory":
+		storage = &sqlstorage.Storage{}
+	case "db":
+		storage = &sqlstorage.Storage{}
+	default:
+		logger.Warn("unknown storage type: " + config.Settings.Storage.Type + ", using memory")
+		storage = &sqlstorage.Storage{}
+	}
 
-	calendar := app.New(logg, storage)
+	calendar := app.New(storage)
 
-	server := internalhttp.NewServer(logg, calendar)
+	server := internalhttp.NewServer(calendar, fmt.Sprintf("%v:%v", config.Settings.Server.Host, config.Settings.Server.Port))
 
 	ctx, cancel := signal.NotifyContext(context.Background(),
 		syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
@@ -50,14 +59,14 @@ func main() {
 		defer cancel()
 
 		if err := server.Stop(ctx); err != nil {
-			logg.Error("failed to stop http server: " + err.Error())
+			logger.Error("failed to stop http server: " + err.Error())
 		}
 	}()
 
-	logg.Info("calendar is running...")
+	logger.Info("calendar is running...")
 
 	if err := server.Start(ctx); err != nil {
-		logg.Error("failed to start http server: " + err.Error())
+		logger.Error("failed to start http server: " + err.Error())
 		cancel()
 		os.Exit(1) //nolint:gocritic
 	}
--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -1,38 +1,81 @@
 package logger
 
 import (
-	"bytes"
-	"log/slog"
-	"strings"
+	"io"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-func New(level string) *slog.Logger {
-	var logLevel = &slog.LevelVar{}
+type Logger struct {
+	core   *zap.Logger
+	level  string
+	writer io.Writer
+}
 
-	var logBuffer = new(bytes.Buffer)
+var myLog *Logger
 
-	opts := &slog.HandlerOptions{
-		Level: logLevel,
-	}
+const (
+	ErrorLevel = "ERROR"
+	WarnLevel  = "WARN"
+	InfoLevel  = "INFO"
+	DebugLevel = "DEBUG"
+)
 
-	handler := slog.NewJSONHandler(logBuffer, opts)
-	logger := slog.New(handler)
-	slog.SetDefault(logger)
+func init() {
+	myLog = &Logger{core: zap.Must(zap.NewDevelopment()), level: InfoLevel, writer: os.Stdout}
+	initCore()
+}
 
-	level = strings.ToLower(level)
+func SetLogLevel(level string) {
+	myLog.level = level
+	initCore()
+}
 
-	switch level {
-	case "debug":
-		logLevel.Set(slog.LevelDebug)
-	case "info":
-		logLevel.Set(slog.LevelInfo)
-	case "warn":
-		logLevel.Set(slog.LevelWarn)
-	case "error":
-		logLevel.Set(slog.LevelError)
+func SetWriter(writer io.Writer) {
+	myLog.writer = writer
+	initCore()
+}
+
+func initCore() {
+	var zapLevel zapcore.Level
+
+	switch myLog.level {
+	case "WARN":
+		zapLevel = zap.WarnLevel
+	case "INFO":
+		zapLevel = zap.InfoLevel
+	case "DEBUG":
+		zapLevel = zap.DebugLevel
 	default:
-		logLevel.Set(slog.LevelInfo)
+		zapLevel = zap.ErrorLevel
 	}
 
-	return logger
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.AddSync(myLog.writer),
+		zap.NewAtomicLevelAt(zapLevel))
+
+	myLog.core = zap.New(core)
+}
+
+func GetLogger() *Logger {
+	return myLog
+}
+
+func Error(msg string) {
+	myLog.core.Error(msg)
+}
+
+func Warn(msg string) {
+	myLog.core.Warn(msg)
+}
+
+func Info(msg string) {
+	myLog.core.Info(msg)
+}
+
+func Debug(msg string) {
+	myLog.core.Debug(msg)
 }
